67ecab8cfd97e70e3cc36bde37b029e90181c6e9
iliassanati <iliassanati@gmail.com>
2022-04-11 00:28:24 +0000

    110422:2

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,17 +3,25 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
@@ -25,7 +33,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
